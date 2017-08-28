64979233502263e7aae47c33811b259eae9bd7f6
robcbryant <robcbryant@gmail.com>
2017-08-28 12:48:55 -0400

    Signed-off-by: robcbryant <robcbryant@gmail.com>

---
+++ maqluengine/urls.py
@@ -0,0 +1,8 @@
+"""urlconf for the maqlu-engine application"""
+
+from django.conf.urls import url, patterns, include
+
+urlpatterns = patterns('maqluengine.views',
+   
+
+)
---
+++ projectanka/urls.py
@@ -0,0 +1,27 @@
+
+from django.conf import settings
+from django.conf.urls import include, patterns, url
+from django.contrib import admin
+admin.autodiscover()
+
+#from maqluengine.admin import admin_site
+
+def bad(request):
+    """ Simulates a server error """
+    1 / 0
+
+urlpatterns = patterns('',
+    # Examples:
+
+
+    url(r'', include('maqluengine.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^bad/$', bad),
+)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += patterns('',
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    )
